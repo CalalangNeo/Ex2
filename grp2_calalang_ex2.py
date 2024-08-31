c2607f512463601855d03028ece43494c25225d9
CalalangNeo <143771557+CalalangNeo@users.noreply.github.com>
2024-08-31 19:02:48 +0800

    Update grp2_calalang_ex2.py

--- grp2_calalang_ex2.py
+++ grp2_calalang_ex2.py
@@ -1,8 +1,53 @@
+#Task 1: Performs Arithmetic Operations
+print("Task 1: Arithmetic Operations")
 
-# Assign integer literals to num1 and num2
+#Assigned two variables
+a = 9
+b = 4
+print("a =",a)
+print("b =",b)
+
+#Addition Operation
+result = a + b
+#Display the result
+print("Addition Operation:", f"{a} + {b} = {result}")
+
+#Subtraction Operation
+result = a - b
+print("Subtraction Operation:", f"{a} - {b} = {result}")
+
+#Division Operation
+result = a / b
+print("Division Operation:", f"{a} / {b} = {result}")
+
+#Multiplication Operation
+result = a * b
+print("Multiplication Operation:", f"{a} * {b} = {result}")
+
+#Modulus Operation
+result = a % b
+print("Modulus Operation:", f"{a} % {b} = {result}")
+
+#Exponential Operation
+result = a ** b
+print("Exponential Operation:", f"{a} ** {b} = {result}")
+
+#Task 2: Integer with and without underscores
+print("Task 2: Integer Literals with Underscores")
 num1 = 25_000_000
-num2 = 25000000
+num2 = 25000000 
 
-# Print num1 and num2 on separate lines
-print(num1)
-print(num2)
+print("num1:", num1)
+print("num2:", num2)
+
+#Task 3: Data types
+print("Task 3: Data Types")
+#creating variables
+num1 = 23
+num2 = 0.22
+num3 = 7+3j
+
+#checking the data Types
+print("num1:", num1, 'is type of', type(num1))
+print("num2:", num2, 'is type of', type(num2))
+print("num3:", num3, 'is type of', type(num3))
